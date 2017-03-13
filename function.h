72e9b49084f52cfa44e8731ef4e593952087aeec
Unknown <delait7@gmail.com>
2017-03-13 13:53:41 +0100

    show game status

--- function.h
+++ function.h
@@ -2,10 +2,26 @@
 
 using namespace std;
 
-void generate{
+void generate(){
 	
 }
 
+void ShowGame(int **hist){
+	for(int i = 0; i<9; i++){
+		for(int j = 0; j < 4; j++){
+			cout << hist[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
 void newgame(){
-	for()
+	//int tab[] = generate();
+	int history[9][4];
+	for(int i = 0; i<9; i++)
+		for(int j = 0; j < 4; j++)
+			history[i][j] = 0;	
+	
+	ShowGame(history);
+	generate();
 }
