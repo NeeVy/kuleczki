dda9b0ad875991c66919f64b7a63a92764d3a198
Unknown <wojtek_wzi@wp.pl>
2017-03-13 13:52:28 +0100

    bleble

--- function.h
+++ function.h
@@ -2,10 +2,29 @@
 
 using namespace std;
 
-void generate{
-	
+int generate(){
+	srand(time(NULL));
+	for(int i=0; i<4; i++){
+		p[i]=rand()%8+1;
+	}
+	return p[];
+}
+
+void hack(){
+	cout<<"wygenerowane liczby"<<endl;
+	for (int i=0; i<4; i++){
+		cout<<p[i]<<"\t";
+	}
 }
 
 void newgame(){
-	for()
+	;
 }
+
+void wpisz(){
+	for(int i=0; i<4; i++){
+		cout<<"podaj"<<" "<<i<<" liczbe"<<endl;
+		cin>>p[i];
+	}
+}
+
